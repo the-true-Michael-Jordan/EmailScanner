8554977c12749ae208679ad5b3736a1727a3db40
Michael Kurowski <Byamarro@gmail.com>
2018-01-06 01:10:50 +0100

    Extracted findPasswordsInBlob to separated function

---
+++ utils.js
@@ -0,0 +1,20 @@
+module.exports = {
+    findPasswordsInBlob(email, blob) {
+        if(blob.includes(email)){
+            let passwords = []
+            let lastFoundEmail = 0
+            while (true) {
+                const emailPosition = blob.indexOf(email, lastFoundEmail)
+                if (emailPosition === -1) break
+                const endOfLinePosition = blob.indexOf('\n', emailPosition)
+                const emailPasswordPair = blob.slice(emailPosition, endOfLinePosition)
+                const password = emailPasswordPair.split(':')[1]
+                passwords.push(password)
+                lastFoundEmail = endOfLinePosition
+            }
+            return passwords
+        } else {
+            return []
+        }
+    }
+}
